39f08e33a4e9b9547ee10df2d64a1ba1e5dcd3ed
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-09-12 15:32:11 +0200

    feat: create folder and add bootstrap..

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "Hello world",
+    };
+  },
+  // components: {
+  //   MyComponent,
+  // },
+};
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style lang="scss" scoped></style>
